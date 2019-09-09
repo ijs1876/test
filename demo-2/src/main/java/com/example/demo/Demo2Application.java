47f399f836d677e02fc05e9ddc09b7ead5ae92ee
ijs1876 <i1876@naver.com>
2019-09-09 18:06:00 +0900

    111

---
+++ demo-2/src/main/java/com/example/demo/Demo2Application.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+public class Demo2Application {
+
+	@RequestMapping ("/hello")
+	public @ResponseBody String index() {
+		return "Hello";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Demo2Application.class, args);
+	}
+
+
+}
